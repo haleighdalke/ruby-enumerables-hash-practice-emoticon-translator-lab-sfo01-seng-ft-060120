5e94d67b3b8ad39710bd5a11a2b936615d207f19
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 20:22:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,9 +17,7 @@ def get_japanese_emoticon(file_path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
-  (file_path, emoticon)
+def get_english_meaning(file_path, emoticon)
   # code goes here
   emoticon_hash = load_library(file_path)
   emoticon_hash.each do |k, v|
