dc5850fc58305bc82fe19cf0e51c9e8ffaecdc58
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 20:27:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 
 def load_library(file_path)
   # code goes here
-  emoticon_hash = file_path.load_library
+  emoticon_hash = YAML.load_file(file_path)
   emoticon_hash.reduce({}) do |memo, (k, v)|
     memo[k] = {
       :english => v[0],
