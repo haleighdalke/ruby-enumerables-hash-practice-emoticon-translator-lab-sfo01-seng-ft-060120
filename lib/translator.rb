ce955613711fbcf381bf9be91779c638fa93512f
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 20:21:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,30 @@ def load_library
   # code goes here
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  emoticon_hash = load_library(file_path)
+  emoticon_hash.each do |k, v|
+    if v[:english] == emoticon
+      #binding.pry
+      return v[:japanese]
+    end
+    #binding.pry
+  end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
   # code goes here
+  (file_path, emoticon)
+  # code goes here
+  emoticon_hash = load_library(file_path)
+  emoticon_hash.each do |k, v|
+    if v[:japanese] == emoticon
+      #binding.pry
+      return k
+    end
+    #binding.pry
+  end
+  return "Sorry, that emoticon was not found"
 end
